388f58d6bc1d0a398b6ccb329968700aeb70cc7a
16602767040@163.com <16602767040@163.com>
2019-06-03 17:25:02 +0800

    swj

---
+++ src/test/java/com/example/ubun/UbunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ubun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UbunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
